ed8df69ff06497421b780c159a9419eaf42e0164
GRoCk3t <40119925+GRoCk3t@users.noreply.github.com>
2019-04-07 19:49:55 +0200

    Add files via upload

---
+++ keyboards/alpha/keymaps/paroalpha/keymap.c
@@ -0,0 +1,14 @@
+//Tap Dance Declarations
+enum {
+  TD_ESC_CAPS = 0
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
+// Other declarations would go here, separated by commas, if you have them
+};
+
+//In Layer declaration, add tap dance item in place of a key code
+TD(TD_ESC_CAPS)
